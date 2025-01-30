ea6ac187efe0781b311cbc612d03b41eaea02601
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-01-31 02:17:40 +0330

    ⚗️ Create a simple script using crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let random_number = rand::rng().random_range(1..=10);
+    
+    loop {
+        let mut guess = String::new();
+        println!("Enter you guess!");
+        println!("secret number is :{}", random_number);
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+            
+        };
+
+        println!("your guess is :{}", guess);
+
+
+        match guess.cmp(&random_number) {
+            Ordering::Equal => {
+                println!("Done!");
+                break;
+            },
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!")
+        };
+    };
 }
